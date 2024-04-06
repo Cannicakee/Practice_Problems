fe6e0e2029be3a5025553ae8605146c0d9b98326
Cannicakee <stanfill11@gmail.com>
2024-04-06 16:32:12 -0600

    Incomplete Problem 1

--- aa02-additional-problems/pt-4/problems/01-best-student.js
+++ aa02-additional-problems/pt-4/problems/01-best-student.js
@@ -17,10 +17,23 @@
 */
 
 const bestStudent = (...args) => {
-    // Your code here 
+    if(args === undefined) {
+        return null;
+    }
+    let max = 0;
+    for(let key of args) {
+        if(key.points > max)
+        max = key.points
+    }
 }
 
+const student1 = { name: "Geoff", points: 50 };
+    const student2 = { name: "Justin", points: 1000 };
+    const student3 = { name: "Tom", points: 568 };
+    const student4 = { name: "Emily", points: 700 };
+    const student5 = { name: "Corina", points: 750 };
 
+    console.log(bestStudent(student1, student2, student3, student4, student5)) // returns "Justin"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
     module.exports = bestStudent;
