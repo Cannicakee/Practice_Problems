7dde43bc39a0545df0e2096aee43681f47bd2861
Cannicakee <stanfill11@gmail.com>
2024-04-06 19:59:58 -0600

    Solved Problem 3

--- aa02-additional-problems/pt-4/problems/03-discount-prices.js
+++ aa02-additional-problems/pt-4/problems/03-discount-prices.js
@@ -13,8 +13,22 @@
 
 */
 
-// Your code here 
+const discountPrices = (prices, discounts) => {
+    const arr = [];
+    prices.forEach((value1, index) => {
+        const value2 = discounts[index];
+        const product = value1 * value2;
+        const discount = value1 - product;
+        const changedPriced = (discount).toFixed(2);
+        const dollarSign = "$" + changedPriced;
+        arr.push(dollarSign);
+        
+    })
+    return arr;
+}
 
+console.log(discountPrices([10.00, 21.99, 54.49, 60.00], [0.2, 0.15, 0.3, 0.5])) // prints [ '$8.00', '$18.69', '$38.14', '$30.00' ]
+console.log(discountPrices([4.99, 12.29, 59.00], [0.15, 0.3, 0.1])) // prints [ '$4.24', '$8.60', '$53.10' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
     module.exports = discountPrices;
